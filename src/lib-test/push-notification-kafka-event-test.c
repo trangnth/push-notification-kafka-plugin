d87064da742c3fdd9ca9c639b6135d73ee4e2d07
trangnth <trangnguyenthihuyen@vccorp.vn>
2021-03-29 17:32:07 +0700

    feat: edit dovecot log format and data push to kafka

--- src/lib-test/push-notification-kafka-event-test.c
+++ src/lib-test/push-notification-kafka-event-test.c
@@ -180,7 +180,7 @@ static void write_msg_prefix_test(void) {
   event.seq = 1;
 #endif
 
-  string_t *t = write_msg_prefix(&dtxn, "MailboxCreate", &event);
+  string_t *t = write_msg_prefix(&dtxn, "MailboxCreate", &event, 1);
   //  i_info("MSG: %s ", str_c(t));
   test_assert(str_equals(t, test));
   pool_unref(&pool);
@@ -258,7 +258,7 @@ static void write_flags_event_test(void) {
   const char *k4 = "k:old_keyword2";
   const char *const *name4 = &k4;
   array_append(&keywords_old, name4, 1);
-  t = write_flags_event(&dtxn, &ctx, "MailboxCreate", &event, flags, &keywords, flags_old, &keywords_old);
+  t = write_flags_event(&dtxn, &ctx, "MailboxCreate", &event, flags, &keywords, flags_old, &keywords_old, 1);
   // i_info("MSG: %s ", str_c(t));
   test_assert(str_equals(t, test2));
 
@@ -276,7 +276,7 @@ static void write_flags_event_test(void) {
 
   flags |= MAIL_FLAGGED;
   flags_old |= MAIL_ANSWERED;
-  t = write_flags_event(&dtxn, &ctx, "MailboxCreate", &event, flags, &keywords, flags_old, &keywords_old);
+  t = write_flags_event(&dtxn, &ctx, "MailboxCreate", &event, flags, &keywords, flags_old, &keywords_old, 1);
   // i_info("MSG: %s ", str_c(t));
 
   test_assert(str_equals(t, test3));
@@ -336,7 +336,7 @@ static void write_event_messagenew_test(void) {
   event.event = &event_cfg;
   struct push_notification_txn_event *const ev = &event;
 
-  string_t *t = write_event_messagenew(&dtxn, &msg, &ev);
+  string_t *t = write_event_messagenew(&dtxn, &msg, &ev, 1);
   // i_info("MSG: %s ", str_c(t));
   test_assert(str_equals(t, test));
 
