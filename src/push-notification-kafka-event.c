92709856d0d587b347036e98580d42b10b161916
trangnth <trangnguyenthihuyen@vccorp.vn>
2021-03-29 10:20:25 +0700

    feat: edit output log format

--- src/push-notification-kafka-event.c
+++ src/push-notification-kafka-event.c
@@ -218,7 +218,7 @@ string_t *write_event_messagenew(struct push_notification_driver_txn *dtxn, stru
 
   if (data->date != -1) {
     struct tm *tm = gmtime(&data->date);
-    str_printfa(str, ",\"date\":\"%s\",\"date2\":\"%s\"", iso8601_date_create_tm(tm, data->date_tz),  asctime(&tm));
+    str_printfa(str, ",\"date\":\"%s\",\"date2\":\"%s\"", iso8601_date_create_tm(tm, data->date_tz), data->date_tz);
   }
 
   if (data->from != NULL) {
@@ -262,7 +262,7 @@ string_t *write_event_messageappend(struct push_notification_driver_txn *dtxn, s
 
   if (data->date != -1) {
     struct tm *tm = gmtime(&data->date);
-    str_printfa(str, ",\"date\":\"%s\",\"date2\":\"%s\"", iso8601_date_create_tm(tm, data->date_tz), data->date_tz);
+    str_printfa(str, ",\"date\":\"%s\"", iso8601_date_create_tm(tm, data->date_tz));
   }
 
   if (data->from != NULL) {
@@ -289,11 +289,11 @@ string_t *write_event_messageappend(struct push_notification_driver_txn *dtxn, s
     str_append(str, "\"");
   }
 
-  if (data->message_id != NULL) {
-    str_append(str, ",\"message_id\":\"");
-    json_append_escaped(str, data->message_id);
-    str_append(str, "\"");
-  }
+  // if (data->message_id != NULL) {
+  //   str_append(str, ",\"message_id\":\"");
+  //   json_append_escaped(str, data->message_id);
+  //   str_append(str, "\"");
+  // }
 
   str_append(str, "}");
   return str;
