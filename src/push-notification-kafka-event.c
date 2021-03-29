1e50dea16d058249d79643079b1ee7ffc60ef85b
trangnth <trangnguyenthihuyen@vccorp.vn>
2021-03-29 08:59:09 +0700

    feat: edit write_event_messageappend function

--- src/push-notification-kafka-event.c
+++ src/push-notification-kafka-event.c
@@ -255,7 +255,7 @@ string_t *write_event_messageappend(struct push_notification_driver_txn *dtxn, s
 
   if (data->date != -1) {
     struct tm *tm = gmtime(&data->date);
-    str_printfa(str, ",\"date\":\"%s\"", iso8601_date_create_tm(tm, data->date_tz));
+    str_printfa(str, ",\"date\":\"%s\",\"date2\":\"%s\"", iso8601_date_create_tm(tm, data->date_tz), data->date_tz);
   }
 
   if (data->from != NULL) {
