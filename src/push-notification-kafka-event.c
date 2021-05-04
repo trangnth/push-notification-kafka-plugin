af2d8f0e498c934e18e39320d43889e67745cd1c
trangnth <trangnguyenthihuyen@vccorp.vn>
2021-05-04 14:52:38 +0700

    debug flag_set event

--- src/push-notification-kafka-event.c
+++ src/push-notification-kafka-event.c
@@ -125,7 +125,7 @@ bool write_flags(enum mail_flags flags, string_t *str) {
   }
 
   // i_debug("%sTRANGG - flags: %s", LOG_LABEL, flags);
-  i_debug("%sTRANGG - flag_written: %s", LOG_LABEL, flag_written);
+  // i_debug("%sTRANGG - flag_written: %s", LOG_LABEL, flag_written);
 
   return flag_written;
 }
